74750d855543179005c24d9fb3a6f15de3a56306
ozPop <ozziepopovas@gmail.com>
2016-11-29 16:28:27 -0500

    add mocha.opts, update #checkCollision

--- index.js
+++ index.js
@@ -21,20 +21,14 @@ function checkCollision(rock) {
   // implement me!
   // use the comments below to guide you!
   const top = positionToInteger(rock.style.top)
-
   // rocks are 20px high
   // DODGER is 20px high
   // GAME_HEIGHT - 20 - 20 = 360px;
   if (top > 360) {
-    const dodgerLeftEdge = positionToInteger(DODGER.style.left)
-
-    // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
-    const dodgerRightEdge = 0;
-
-    const rockLeftEdge = positionToInteger(rock.style.left)
-
-    // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
-    const rockRightEdge = 0;
+    const dodgerLeftEdge = positionToInteger(DODGER.style.left);
+    const dodgerRightEdge = dodgerLeftEdge + 40;
+    const rockLeftEdge = positionToInteger(rock.style.left);
+    const rockRightEdge = rockLeftEdge + 20;
 
     if (false /**
                * Think about it -- what's happening here?
@@ -48,6 +42,11 @@ function checkCollision(rock) {
                */) {
       return true
     }
+    return (
+            rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge || 
+            rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge ||
+            rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge
+            );
   }
 }
 
